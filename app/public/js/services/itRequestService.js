4b9671f63a309f9ce21d51b7da29b1933407d252
Reza Afzalan <RAfzalan@gmail.com>
2015-11-25 14:49:42 +0330

    rm bug

--- app/public/js/services/itRequestService.js
+++ app/public/js/services/itRequestService.js
@@ -194,10 +194,4 @@ dashboardApp.service('itRequestService', function($http, $sce){
         });
     };
     
-    function PopupCenter(pageURL, title, w, h) {
-        var left = (screen.width/2)-(w/2);
-        var top = 0;
-        var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
-        return targetWin;
-    };     
 });
