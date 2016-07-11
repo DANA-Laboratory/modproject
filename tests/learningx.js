f8e1fa1f042b73547306c165d9bbc6f6e7e0ac37
vimalaad <vimalaad@gmail.com>
2016-07-11 07:05:01 +0430

    table design

---
+++ tests/learningx.js
@@ -0,0 +1,103 @@
+/**
+ * Created by AliReza on 7/6/2016.
+ */
+
+var modelsSqlite3 = require('../app/models-sqlite3');
+var fs = require('fs');
+var assert = require('chai').assert;
+var dbpath = __dirname + '/learnxdb.sqlite';
+var basedb = new (modelsSqlite3.basedb)(dbpath);
+var ddl = `
+    --
+    -- File generated with SQLiteStudio v3.0.6 on چهارشنبه ژوئيه 6 11:12:41 2016
+    --
+    -- Text encoding used: UTF-8
+    --
+    PRAGMA foreign_keys = off;
+    BEGIN TRANSACTION;
+    
+    -- Table: tblStatements
+    CREATE TABLE tblStatements (ID INTEGER PRIMARY KEY AUTOINCREMENT, Actor INTEGER REFERENCES tblActors (ID), Object INTEGER REFERENCES tblObjectTypes (ID), Verb INTEGER REFERENCES tblVerbs (ID), Time BIGINT NOT NULL, Attributes STRING);
+    
+    -- Table: tblClass
+    CREATE TABLE tblClass (ID INTEGER PRIMARY KEY AUTOINCREMENT, TimeStart BIGINT NOT NULL, TimeEnd BIGINT NOT NULL, Course INTEGER REFERENCES tblCourse (ID), Attribute STRING);
+    
+    -- Table: tblStatementTypes
+    CREATE TABLE tblStatementTypes (ID INTEGER PRIMARY KEY AUTOINCREMENT, VerbID INTEGER REFERENCES tblVerbs (ID) NOT NULL, ActorType INTEGER REFERENCES tblActorTypes (ID) NOT NULL, ObjectType INTEGER REFERENCES tblObjectTypes (ID) NOT NULL, DateCaption STRING, Description STRING NOT NULL, AttributeType STRING);
+    INSERT INTO tblStatementTypes (ID, VerbID, ActorType, ObjectType, DateCaption, Description, AttributeType) VALUES (1, 1, 2, 1, 'زمان ورود', 'ورود کارآموز به محل کارآموزی یا کارورزی', NULL);
+    INSERT INTO tblStatementTypes (ID, VerbID, ActorType, ObjectType, DateCaption, Description, AttributeType) VALUES (2, 2, 2, 1, 'زمان خروج', 'خروج کارآموز از محل کارآموزی یا کارورزی به دلیل ترخیص، اتمام دوره یا اخراج', '{status:["ترخیص","اخراج"]}');
+    INSERT INTO tblStatementTypes (ID, VerbID, ActorType, ObjectType, DateCaption, Description, AttributeType) VALUES (3, 3, 2, 2, 'زمان قرار گفتن', 'گروه بندی', NULL);
+    INSERT INTO tblStatementTypes (ID, VerbID, ActorType, ObjectType, DateCaption, Description, AttributeType) VALUES (4, 4, 2, 3, 'زمان شروع', 'شرکت در کلاس آموزشی', NULL);
+    INSERT INTO tblStatementTypes (ID, VerbID, ActorType, ObjectType, DateCaption, Description, AttributeType) VALUES (5, 5, 2, 3, 'زمان اتمام', 'اتمام کلاس آموزشی', '{result:0, status:["قبول","رد"]}');
+    INSERT INTO tblStatementTypes (ID, VerbID, ActorType, ObjectType, DateCaption, Description, AttributeType) VALUES (6, 6, 2, 3, 'تاریخ غیبت', 'غیبت در کلاس آموزشی', NULL);
+    INSERT INTO tblStatementTypes (ID, VerbID, ActorType, ObjectType, DateCaption, Description, AttributeType) VALUES (7, 7, 2, 3, 'زمان تنظیم', 'تنظیم روکش حق التدریس', NULL);
+    INSERT INTO tblStatementTypes (ID, VerbID, ActorType, ObjectType, DateCaption, Description, AttributeType) VALUES (8, 8, 1, 3, 'زمان شروع', 'تدریس کلاس آموزشی', NULL);
+    INSERT INTO tblStatementTypes (ID, VerbID, ActorType, ObjectType, DateCaption, Description, AttributeType) VALUES (9, 9, 1, 4, 'زمان ارزیابی', 'تکیل فرم ارزیابی کارآموزی', '{result:{}}');
+    INSERT INTO tblStatementTypes (ID, VerbID, ActorType, ObjectType, DateCaption, Description, AttributeType) VALUES (10, 10, 2, 5, 'زمان آزمون ', 'طی کردن آزمون', '{result:{}}');
+    INSERT INTO tblStatementTypes (ID, VerbID, ActorType, ObjectType, DateCaption, Description, AttributeType) VALUES (11, 6, 2, 1, 'تاریخ غیبت', 'غیبت در محل', NULL);
+    
+    -- Table: tblActorTypes
+    CREATE TABLE tblActorTypes (ID INTEGER PRIMARY KEY AUTOINCREMENT, Caption STRING NOT NULL);
+    INSERT INTO tblActorTypes (ID, Caption) VALUES (1, 'مدرس');
+    INSERT INTO tblActorTypes (ID, Caption) VALUES (2, 'کارآموز');
+    
+    -- Table: tblObjects
+    CREATE TABLE tblObjects (ID INTEGER PRIMARY KEY AUTOINCREMENT, Type INTEGER NOT NULL REFERENCES tblObjectTypes (ID), ForeignKey INTEGER, Atributes STRING);
+    
+    -- Table: tblGroups
+    CREATE TABLE tblGroups (ID INTEGER PRIMARY KEY AUTOINCREMENT, Caption TEXT NOT NULL);
+    
+    -- Table: tblActors
+    CREATE TABLE tblActors (ID INTEGER PRIMARY KEY AUTOINCREMENT, Type INTEGER REFERENCES tblActorTypes (ID), Name STRING, Family STRING, Code STRING, Attributes STRING);
+    
+    -- Table: tblGrouping
+    CREATE TABLE tblGrouping (ID INTEGER PRIMARY KEY AUTOINCREMENT, "Group" INTEGER NOT NULL REFERENCES tblGroups (ID), Actors STRING);
+    
+    -- Table: tblCourse
+    CREATE TABLE tblCourse (ID INTEGER PRIMARY KEY AUTOINCREMENT, Caption TEXT NOT NULL, Attributes STRING);
+    
+    -- Table: tblVerbs
+    CREATE TABLE tblVerbs (ID INTEGER PRIMARY KEY AUTOINCREMENT, Caption STRING NOT NULL);
+    INSERT INTO tblVerbs (ID, Caption) VALUES (1, 'وارد شد');
+    INSERT INTO tblVerbs (ID, Caption) VALUES (2, 'خارج شد ');
+    INSERT INTO tblVerbs (ID, Caption) VALUES (3, 'گروه بندی شد ');
+    INSERT INTO tblVerbs (ID, Caption) VALUES (4, 'شرکت کرد');
+    INSERT INTO tblVerbs (ID, Caption) VALUES (5, 'تمام کرد ');
+    INSERT INTO tblVerbs (ID, Caption) VALUES (6, 'غیبت کرد');
+    INSERT INTO tblVerbs (ID, Caption) VALUES (7, 'روکش تنظیم شد');
+    INSERT INTO tblVerbs (ID, Caption) VALUES (8, 'تدریس کرد');
+    INSERT INTO tblVerbs (ID, Caption) VALUES (9, 'ارزیابی شد');
+    INSERT INTO tblVerbs (ID, Caption) VALUES (10, 'آزمون داد ');
+    
+    -- Table: tblObjectTypes
+    CREATE TABLE tblObjectTypes (ID INTEGER PRIMARY KEY AUTOINCREMENT, Caption STRING NOT NULL);
+    INSERT INTO tblObjectTypes (ID, Caption) VALUES (1, 'محل');
+    INSERT INTO tblObjectTypes (ID, Caption) VALUES (2, 'گروه');
+    INSERT INTO tblObjectTypes (ID, Caption) VALUES (3, 'کلاس');
+    INSERT INTO tblObjectTypes (ID, Caption) VALUES (4, 'فرم ارزیابی');
+    INSERT INTO tblObjectTypes (ID, Caption) VALUES (5, 'فرم آزمون');
+    
+    COMMIT TRANSACTION;
+    PRAGMA foreign_keys = on;
+`;
+
+describe('do import', function() {
+    before(function (done) {
+        if (fs.existsSync(dbpath)) {
+            fs.unlinkSync(dbpath);
+        }
+        basedb.createdb(ddl, function (err) {
+            assert.isNull(err);
+            done();
+        })
+    });
+    it('basedb could connect', function (done) {
+        done();
+    });
+    after(function (done) {
+        basedb.disconnect(function (err) {
+            fs.unlinkSync(dbpath);
+            done();
+        })
+    });
+});
